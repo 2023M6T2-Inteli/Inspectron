73c3b4bb046381224ff75a05f7e8b0149201eb12
Lyorrei Shono Quintão <99191948+lyorrei-inteli@users.noreply.github.com>
2023-05-15 11:39:26 -0300

    Página de início feita (login)

--- src/frontend/src/components/input/index.tsx
+++ src/frontend/src/components/input/index.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import Button, { ButtonTypes } from "../button";
 import { FieldError, FieldErrorsImpl, Merge } from "react-hook-form";
+import { LucideIcon } from "lucide-react";
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
     label?: string;
     error?: string;
+    Icon?: LucideIcon;
 }
 
-const Input: React.FC<Props> = ({ label, error, ...rest }) => {
+const Input: React.FC<Props> = ({ label, error, Icon, ...rest }) => {
     return (
         <div>
-            {label && <label className=" text-lg block">{label}</label>}
-            <input className="py-1 outline-none border-b-2 w-full" {...rest} />
+            {label && <label className="text-[#7A7A96] text-lg mb-1 block">{label}</label>}
+            <div className="border-2 p-3 w-full flex items-center gap-4 text-[#B9BCC7] rounded-lg">
+                {Icon && <Icon  />}
+                <input
+                    className="grow py-1 bg-transparent outline-none placeholder-[#B9BCC7] text-gray-950"
+                    {...rest}
+                />
+            </div>
             {error && <span className="text-red-800 mt-1 block">{error}</span>}
         </div>
     );
